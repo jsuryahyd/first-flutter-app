123599d2e253c61dbe8174d443d2ae0262af5b82
Jaya Surya <jaysuryahyd@gmail.com>
2018-08-26 17:00:54 +0530

    some changes

--- lib/Products.dart
+++ lib/Products.dart
@@ -5,26 +5,40 @@ class Products extends StatelessWidget {
   Products(this.items);
 
   Widget build(BuildContext context) {
-    Widget productsWidget =  Center(child:Text('No items yet.'));
+    return _buildProductsList(items);
+  }
+
+  _buildProductsList(items) {
     print(items);
-    if(items.length > 0 && items.length < 4){
+
+    Widget productsWidget =
+        Container(); //just so we dont return null if all cases fail.
+    if (items.length == 0) {
+      productsWidget = Center(child: Text('No items yet.'));
+    } else if (items.length > 0 && items.length < 4) {
       productsWidget = Column(
-      children: items
-          .map((item) => Card(child:new Padding( padding:EdgeInsets.all(5.0),
-                child: Column(children: <Widget>[
-                  Image.asset('assets/traditional-lunch.jpg'),
-                  Text(item)
-                ]),)
-              ))
-          .toList(),
-    );
-    }else if(items.length >= 4){
-      productsWidget = ListView.builder(itemBuilder: (BuildContext context,int index){
-        return Card(child:Column(children:[
-          Image.asset('assets/traditional-lunch.jpg'),
-          Text(items[index])
-        ]));
-      },itemCount: items.length,);
+        children: items
+            .map((item) => Card(
+                    child: new Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Column(children: <Widget>[
+                    Image.asset('assets/traditional-lunch.jpg'),
+                    Text(item)
+                  ]),
+                )))
+            .toList(),
+      );
+    } else if (items.length >= 4) {
+      productsWidget = ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+              child: Column(children: [
+            Image.asset('assets/traditional-lunch.jpg'),
+            Text(items[index])
+          ]));
+        },
+        itemCount: items.length,
+      );
     }
     return productsWidget;
   }
